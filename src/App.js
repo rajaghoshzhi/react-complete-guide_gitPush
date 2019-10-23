506dfc6e9157d382a14b6f181fcdf99200e367e2
Raja Ghosh -X (rajaghos - ZENSAR TECHNOLOGIES INC at Cisco) <rajaghos@partnet.cisco.com>
2019-10-23 15:47:36 +0530

    react project updates

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import classes from './App.css';
 import Person from './components/Persons/person';
 
 class App extends Component {
-  render() {
-    return (
 
+  state = {
+    person:[
+      {key:'r1', name:'raja', age:'30'},
+      {key:'m1', name:'mohit', age:'31'},
+      {key:'a1', name:'Anupamma', age: '27'},
+
+    ],
+    showPerson: false
+  }
+  render() {
+      let enableChild = '';
+      
+      const personArr =  this.state.person.map((obj)=>{
+       if(obj.name === 'Anupamma'){
+        enableChild = 'I love to travel the world.';
+       }
+        return <Person name={obj.name} age={obj.age}>{enableChild}</Person>   
+      })
+
+    return (
       <div className={classes.App}>
-        <div className={[classes.fstyle,classes.fbold].join(' ')}>Initialize React...</div>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <p></p>
+        <div className={[classes.fstyle,classes.fbold].join(' ')}>Initializing React...</div>
+        <button className={classes.button_dsgn}>Toggle Selection</button>
+        {personArr}
       </div>
     );
   }
--- src/components/Persons/person.js
+++ src/components/Persons/person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './person.css';
 
-const Person  = () => {
+const Person  = (props) => {
 
     return (
-        <div>
-            <input type='text'/>
-            <p className={classes.lableName}>My Name is:</p>
+        <div className = {classes.decorateBox}>
+            <p className={classes.lableName}> Enter your Details:</p>
+            <input type='text' value={props.name}/>
+            <p className={classes.lableName}>My Name is {props.name} and my age is {props.age}. {props.children}</p>
         </div>
         
         
