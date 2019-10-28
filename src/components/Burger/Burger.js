9909df1536eaa2fce9111903674e760f3d552c1a
Raja Ghosh -X (rajaghos - ZENSAR TECHNOLOGIES INC at Cisco) <rajaghos@partnet.cisco.com>
2019-10-28 14:25:39 +0530

    code push

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,25 @@ import BurgerIngredient from '../BurgerIngredient/BurgerIngredent';
 
 const Burger = (props) => {
 
+    // console.log(props.ingredients);
+    // Object.entries return like ['salad',1],['meat',2]; transformIngredients return array of objects
+    const transformIngredients = Object.entries(props.ingredients).map((e) => ( { 'name':e[0], 'value':e[1]} ));
+    //  console.log(transformIngredients);
+    
+    let updateRenderPattern =  transformIngredients.map((e,index)=>{
+        var arr = []
+        for(let i=0;i<e.value;i++){
+            arr.push(<BurgerIngredient key={index+'-'+i} type={e.name}></BurgerIngredient>);
+        }
+        
+        console.log(arr);
+         return arr;
+    })
+    console.log(updateRenderPattern)
     return (
         <div className={Classes.Burger}> 
             <BurgerIngredient type='bread-top'></BurgerIngredient>
-            <BurgerIngredient type='salad'></BurgerIngredient>
-            <BurgerIngredient type='cheese'></BurgerIngredient>
-            <BurgerIngredient type='cheese'></BurgerIngredient>
-            <BurgerIngredient type='meat'></BurgerIngredient>
+                {updateRenderPattern}            
             <BurgerIngredient type='bread-bottom'></BurgerIngredient>
         </div>
     )
