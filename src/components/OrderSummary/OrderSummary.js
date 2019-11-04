81a2c0bff31251423f1eb9adeef3e28fb253ee4b
Raja Ghosh -X (rajaghos - ZENSAR TECHNOLOGIES INC at Cisco) <rajaghos@partnet.cisco.com>
2019-11-04 19:12:55 +0530

    react changes

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import Button from '../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ObjPair = Object.keys(props.sendIngredients).map((el)=>{
-        return (           
-            // <Aux>
-                <li key={el}> <span style={{textTransform:'capitalize'}}>{[el]}</span><span style={{float: 'right',marginRight: '100px'}}>{props.sendIngredients[el]}</span> </li>
-            // </Aux>
-        )
-    })
-    console.log(ObjPair);
+class  OrderSummary extends Component {
 
-    return (            
-        <Aux>
-            <h3>Your Order</h3>
-            <p> Yipee!! Delicious burger with following Ingredients:</p>
-            <ul>
-                {ObjPair}
-            </ul>
-            <p><strong>Total Price: {props.currentPrice}</strong></p>
-            <Button btnType='Danger' eventHandler={props.cancelBtn}>Cancel</Button>
-            <Button btnType='Success' eventHandler={props.cancelBtn}>Countinue</Button>
-        </Aux>
-    )
+    constructor(props){
+        super(props);
+    }
+
+    componentDidUpdate(){
+        console.log(" Component Did Update");
+    }
+
+    render() {
+        const ObjPair = Object.keys(this.props.sendIngredients).map((el)=>{
+            return (           
+                // <Aux>
+                    <li key={el}> <span style={{textTransform:'capitalize'}}>{[el]}</span><span style={{float: 'right',marginRight: '100px'}}>{this.props.sendIngredients[el]}</span> </li>
+                // </Aux>
+            )
+        })
+    
+        return (            
+            <Aux>
+                <h3>Your Order</h3>
+                <p> Yipee!! Delicious burger with following Ingredients:</p>
+                <ul>
+                    {ObjPair}
+                </ul>
+                <p><strong>Total Price: {this.props.currentPrice}</strong></p>
+                <Button btnType='Danger' eventHandler={this.props.cancelBtn}>Cancel</Button>
+                <Button btnType='Success' eventHandler={this.props.cancelBtn}>Countinue</Button>
+            </Aux>
+        )
+    }
+    
 }
 
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React from  'react';
+import React, { Component } from  'react';
 import Classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 import Aux from '../../../hoc/Auxillary';
 
 
-const Modal = (props) => {
- console.log(props);
-    return (
-        <Aux>
-            <Backdrop show={props.sendBackdrop} hide={props.removeBackDrop}>
-            </Backdrop>
-            <div className={Classes.Modal}>
-                {props.children}
-            </div>
-        </Aux>      
+class Modal extends Component {
 
-    )
+    shouldComponentUpdate(nextProps, nextState){
+    
+        console.log(" Component Did Update");
+    }
+
+    render (){
+        return (
+            <Aux>
+                <Backdrop show={this.props.sendBackdrop} hide={this.props.removeBackDrop}>
+                </Backdrop>
+                <div className={Classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Aux>      
+    
+        )
+    }
+   
 }
 
 export default Modal;
