7e8f314798dbf208a7e94d8cefea2217f233a499
rajaghosh <rajaghoshzhi@gmail.com>
2023-04-13 11:55:45 +0530

    master text changed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div> hello Raja Ghosh</div>
+        <div> hello Raja Ghosh <span> welcome !</span></div>
         <Layout>
           {/* <BurgerBuilder></BurgerBuilder> */}
           <Switch>           
